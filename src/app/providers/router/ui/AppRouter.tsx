4ae7a30e7599dcb9ac1f2c3baba61365891c6455
InvitationWOW <rozhkolyudmila@yandex.ru>
2025-01-22 22:07:32 +0300

    add jest, add first test

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,9 +1,10 @@
-import { Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => (
-    // eslint-disable-next-line i18next/no-literal-string
     <Suspense fallback={<div>Loading...</div>}>
         <Routes>
             {Object.values(routeConfig).map(({ element, path }) => (
@@ -11,7 +12,6 @@ const AppRouter = () => (
                     key={path}
                     path={path}
                     element={(
-                        // eslint-disable-next-line i18next/no-literal-string
                         <Suspense fallback={<div>Loading...</div>}>
                             <div className="page-wrapper">
                                 {element}
